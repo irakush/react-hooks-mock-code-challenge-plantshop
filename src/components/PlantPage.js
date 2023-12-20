d66b4a5b2ef3b03e1824aa9340cde19edf7b101b
Igor Rakush <irakush@aol.com>
2023-12-20 12:42:24 -0500

    finished Core Deliverables

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const URL = "http://localhost:6001/plants"
+  const [plantArray, setPlantArray] = useState([])
+  const [searchStr, setSerchStr] = useState("")
+
+  console.log(plantArray)
+
+  useEffect(() => {
+    fetch(URL)
+      .then(response => response.json())
+      .then(setPlantArray)
+  }, [])
+
+  const handleSearch = (e) => {
+    setSerchStr(e.target.value)
+  }
+
+  const filteredArray = plantArray.filter(plant => {
+      return plant.name.toLowerCase().includes(searchStr.toLowerCase())
+  })
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm plantArray={filteredArray} setPlantArray={setPlantArray} />
+      <Search handleSearch={handleSearch} />
+      <PlantList plantArray={filteredArray} />
     </main>
   );
 }
